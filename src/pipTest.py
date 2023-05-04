811b78210a1888ce85ed2bcf498854f1c83aaf83
Rodrigue de Guerre <99824035+Dequavious6@users.noreply.github.com>
2023-05-04 10:04:52 +0200

    Create pipTest.py

---
+++ src/pipTest.py
@@ -0,0 +1,17 @@
+class PipTest(list):
+    def __init__(self):
+      pass
+   
+    def hello(self) -> None:
+      print("Hello World!)
+      
+      
+      
+def pipTest() -> None:
+    p = PipTest()
+    p.hello()
+
+if __name__ == "__main__":
+    pipTest()
+  
+__all__ = ['pipTest']
