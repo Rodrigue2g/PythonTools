05c6bc7f4d709ac48773fa35fe68a090bfe96778
Rodrigue de Guerre <99824035+Dequavious6@users.noreply.github.com>
2023-05-04 16:34:11 +0200

    Update pipImport.py

--- pipImport.py
+++ pipImport.py
@@ -35,9 +35,9 @@ import urllib
 import httpimport
 url = "https://raw.githubusercontent.com/Dequavious6/PythonTools/main/src"
 with httpimport.remote_repo(["pipUtility", "copyCatch", "pipTest"], url):
-    from pipUtility import *
-    from copyCatch import *
-    from pipTest import *
+    import pipUtility 
+    import copyCatch 
+    import pipTest 
     
 
 # End of file
