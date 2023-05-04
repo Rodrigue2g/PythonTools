6040577be44844f4a8861315051d0d7cf7ad2d8f
Rodrigue de Guerre <99824035+Dequavious6@users.noreply.github.com>
2023-05-04 11:36:44 +0200

    Update pipTest.py

--- src/pipTest.py
+++ src/pipTest.py
@@ -4,7 +4,7 @@ class PipTest(list):
    
     def hello(self) -> None:
       print("Hello World!")
-      print("Hello World!")
+      #print("Hello World!")
       
       
 def pipTest() -> None:
