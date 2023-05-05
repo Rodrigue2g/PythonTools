ad7bacceba2b3c6749ebd3a6212c4fdde52a0877
Rodrigue de Guerre <99824035+Dequavious6@users.noreply.github.com>
2023-05-05 11:01:54 +0200

    Update pipImport.py

--- pipImport.py
+++ pipImport.py
@@ -47,7 +47,7 @@ class Modules:
         self.li = []
         for key, value in sys.modules.items():
             if "httpimport.HttpImporter" in str(value):
-                if key != "pipImport" && key != "docs":
+                if key != "pipImport" and key != "docs":
                     module = Module(key)
                     setattr(self, key, module)
                     self.li.append(module)
